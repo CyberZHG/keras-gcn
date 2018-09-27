8a606cc36bccf7307f8bd079786eff08414c27f0
CyberZHG <CyberZHG@gmail.com>
2018-09-27 08:19:32 +0800

    Init framework

---
+++ README.md
@@ -0,0 +1,15 @@
+# Keras Graph Convolutional Network
+
+[![Travis](https://travis-ci.org/CyberZHG/keras-gcn.svg)](https://travis-ci.org/CyberZHG/keras-gcn)
+[![Coverage](https://coveralls.io/repos/github/CyberZHG/keras-gcn/badge.svg?branch=master)](https://coveralls.io/github/CyberZHG/keras-gcn)
+[![PyPI](https://img.shields.io/pypi/pyversions/keras-gcn.svg)](https://pypi.org/project/keras-gcn/)
+
+GCN layer.
+
+## Install
+
+```bash
+pip install keras-gcn
+```
+
+## Usage
---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+    name='keras-gcn',
+    version='0.1',
+    packages=['keras_gcn'],
+    url='https://github.com/CyberZHG/keras-gcn',
+    license='MIT',
+    author='CyberZHG',
+    author_email='CyberZHG@gmail.com',
+    description='',
+    long_description=open('README.rst', 'r').read(),
+    install_requires=[
+        'numpy',
+        'Keras',
+    ],
+    classifiers=(
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
