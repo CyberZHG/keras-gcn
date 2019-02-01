21df83220156099d880bf1dd11adaa7eeb13e64c
CyberZHG <CyberZHG@gmail.com>
2019-02-01 08:46:38 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,27 @@
-from setuptools import setup
+import codecs
+from setuptools import setup, find_packages
+
+
+with codecs.open('README.md', 'r', 'utf8') as reader:
+    long_description = reader.read()
+
+
+with codecs.open('requirements.txt', 'r', 'utf8') as reader:
+    install_requires = list(map(lambda x: x.strip(), reader.readlines()))
+
 
 setup(
     name='keras-gcn',
-    version='0.10',
-    packages=['keras_gcn'],
+    version='0.11.0',
+    packages=find_packages(),
     url='https://github.com/CyberZHG/keras-gcn',
     license='MIT',
     author='CyberZHG',
     author_email='CyberZHG@gmail.com',
     description='Graph convolutional layers',
-    long_description=open('README.rst', 'r').read(),
-    install_requires=[
-        'numpy',
-        'Keras',
-    ],
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    install_requires=install_requires,
     classifiers=(
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3.6",
